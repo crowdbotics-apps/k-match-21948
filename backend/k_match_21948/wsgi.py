0f624699caaedf9003048fc4dc427994c09c81b3
Crowdbotics <team@crowdbotics.com>
2020-10-25 17:18:29 +0000

    Update models

--- backend/k_match_21948/wsgi.py
+++ backend/k_match_21948/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'k_match_21948.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "k_match_21948.settings")
 
 application = get_wsgi_application()
